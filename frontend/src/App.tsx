c0a4973e13661b0e83d8ef4365e2c14e5e55e954
Now <now.thongtangsai@gmail.com>
2024-11-03 15:35:24 +0700

    FE: create the graph for the level meter to show channel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import JuceServices from "./api/juce-service";
-
+import Plot from "react-plotly.js";
 function App() {
+  const [rmsLevels, setRmsLevels] = useState<Array<number | null>>([]);
+
   useEffect(() => {
     getExampleResource();
 
-    JuceServices.addEventListener("audioLevels", (event) => {
-      console.log(event);
-    });
+    JuceServices.addEventListener(
+      "audioLevels",
+      (event: Array<number | null>) => {
+        setRmsLevels(event);
+      },
+    );
 
     return () => {
       JuceServices.removeEventListener("audioLevels", () => {
@@ -29,6 +34,39 @@ function App() {
       });
   }
 
+  const processedRmsLevels = rmsLevels.map((level) =>
+    level !== null ? level + 100 : 0,
+  );
+
+  const data = [
+    {
+      x: [`Level 1`, `Level 2`, `Level 3`, `Level 4`],
+      y: processedRmsLevels,
+      base: [-100, -100, -100, -100],
+      type: "bar",
+      marker: { color: "green" },
+      name: "RMS Levels",
+    },
+  ];
+
+  const layout = {
+    title: "RMS Level Meter",
+    yaxis: {
+      title: "Level (dB)",
+      range: [-100, 0], // Set the y-axis range from -100 to 0
+      autorange: false,
+      ticks: "outside",
+      tickvals: [-100, -80, -60, -40, -20, 0],
+      ticktext: ["-100", "-80", "-60", "-40", "-20", "0"],
+    },
+    showlegend: false,
+  };
+
+  const config = {
+    responsive: true, // Makes the chart responsive
+    staticPlot: false, // Makes the chart static
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -45,6 +83,15 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <div style={{ marginTop: "20px" }}>
+        <Plot
+          data={data}
+          layout={layout}
+          style={{ width: "100%", height: "400px" }}
+          config={config}
+        />
+      </div>
     </div>
   );
 }
